3d131bea1720683cca9665e9bf8c6af1c86cf4e8
Campbell Vertesi <campbell@vertesi.com>
2014-04-23 13:04:47 +0200

    adding drupaltest to aliases

---
+++ .drush/drupaltest.aliases.drushrc.php
@@ -0,0 +1,10 @@
+<?php
+// Site aliases for drupaltest
+$aliases['smackdown'] = array(
+  'site' => 'drupaltest',
+  'env' => 'smackdown',
+  'root' => '/var/www/dev.nodesymphony.com/public_html/drupaltest/smackdown',
+  'uri' => 'http://drupaltest.dev.nodesymphony.com/smackdown',
+  'remote-host' => 'drupaltest.dev.nodesymphony.com',
+  'remote-user' => 'cvertesi',
+);
