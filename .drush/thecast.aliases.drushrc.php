0e6e5565d250b92223306ab26d03a4a18aed1ed9
Campbell Vertesi <campbell@vertesi.com>
2014-05-01 23:45:17 +0200

    adding the cast staging to drush aliases

--- .drush/thecast.aliases.drushrc.php
+++ .drush/thecast.aliases.drushrc.php
@@ -4,8 +4,18 @@ $aliases['dev'] = array(
   'parent' => '@parent',
   'site' => 'thecast',
   'env' => 'dev',
-  'root' => '/var/www/dev.nodesymphony.com/public_html/the-cast',
+  'uri' => 'http://the-cast.dev.nodesymphony.com/master',
+  'root' => '/var/www/dev.nodesymphony.com/public_html/the-cast/master',
   'remote-host' => 'dev.nodesymphony.com',
+  'remote-user' => 'cvertesi',
+);
+// Site thecast, environment stage
+$aliases['stage'] = array(
+  'parent' => '@parent',
+  'site' => 'thecast',
+  'env' => 'stage',
+  'root' => '/var/www/staging.the-cast.de/public_html',
+  'remote-host' => 'staging.the-cast.de',
   'remote-user' => 'cvertesi',
 );
 // Site thecast, environment live
