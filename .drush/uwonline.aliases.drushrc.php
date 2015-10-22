a3d461849cd7ca567464712836456c4c4af0ea36
Campbell Vertesi <campbell@vertesi.com>
2015-10-22 14:37:03 +0200

    remove old drush alias files

--- .drush/uwonline.aliases.drushrc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$aliases['uwonline'] = array(
-  'site' => 'uwonline',
-  'root' => '/var/www/vhosts/uwonline.dev/public',
-  'remote-host' => 'dev.forumone.com',
-  'remote-user' => 'cvertesi',
-);
