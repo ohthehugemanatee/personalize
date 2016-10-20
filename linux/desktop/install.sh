bae97f3dd4af4c813988bbd21cd7a190bfc05f15
Campbell Vertesi <campbell@vertesi.com>
2016-10-20 13:20:25 +0200

    add dunst notifications, screenshot tool

--- linux/desktop/install.sh
+++ linux/desktop/install.sh
@@ -5,7 +5,7 @@
 # See README.md for an explanation of each tool.
 sudo add-apt-repository -y ppa:teejee2008/ppa
 
-sudo apt install i3 i3lock suckless-tools i3status dmenu i3lock xbacklight feh conky conky-manager pasystray paman paprefs pavumeter pulseaudio-module-zeroconf pavucontrol variety numlockx lxappearance scrot terminator -y
+sudo apt install i3 i3lock suckless-tools i3status dmenu i3lock xbacklight feh conky conky-manager pasystray paman paprefs pavumeter pulseaudio-module-zeroconf pavucontrol variety numlockx lxappearance xsel terminator -y
 
 #simlink i3 config into place.
 mkdir -p $HOME/.config
