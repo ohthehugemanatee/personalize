3243166eda97073da6df94894e7b240ad384dbf0
Campbell Vertesi <campbell@vertesi.com>
2016-10-23 20:17:49 +0200

    add ynab launcher

---
+++ linux/system/ynab-4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#starts YNAB with WINE. Using a shell script instead of an alias so it show up in the mod+d menu
+
+/usr/bin/wine $HOME/.wine/drive_c/Program\ Files\ \(x86\)/YNAB\ 4/YNAB\ 4.exe
