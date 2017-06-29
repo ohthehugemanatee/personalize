223d6f5e227568d4c889211ddeb1658c1fe34ba4
Campbell Vertesi <campbell@vertesi.com>
2017-06-29 10:19:32 +0200

    Install composer from source rather than apt

--- linux/2017xps13/setup.sh
+++ linux/2017xps13/setup.sh
@@ -57,7 +57,17 @@ ln -sf $PERSONAlIZE/.conky $HOME/.conky
 sudo ln -sf $PERSONAlIZE/screenshot.sh /usr/local/bin/screenshot
 
 # Install applications and tools I like, want, and need.
-sudo apt install -y php phpunit php-mbstring php-sqlite3 openssh-server composer bundler ruby-dev powertop gcc build-essential dropbox steam
+sudo apt install -y php phpunit php-mbstring php-sqlite3 openssh-server bundler ruby-dev powertop gcc build-essential dropbox steam
+
+# Install composer to scripts directory.
+if [ ! -f $HOME/scripts/composer.phar ]; then
+  cd $HOME/scripts
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php composer-setup.php
+  php -r "unlink('composer-setup.php');"
+fi
+sudo ln -sf $HOME/scripts/composer.phar /usr/local/bin/
 
 # Use Overlay FS for docker
 sudo ln -sf $PERSONAlIZE/etc/docker/daemon.json /etc/docker/daemon.json
