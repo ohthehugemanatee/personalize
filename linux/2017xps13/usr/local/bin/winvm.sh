2bbbe40fcd81cebad72f95db8131072bb0e3de23
Campbell Vertesi <campbell@vertesi.com>
2018-03-06 11:30:37 +0100

    Winvm to use zenity gtk windows

--- linux/2017xps13/usr/local/bin/winvm.sh
+++ linux/2017xps13/usr/local/bin/winvm.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Simple script to start/stop my Azure remote workstation.
+# Requires:
+# - Azure CLI configured
+# - Remmina RDP client
+# 
+
+# Change these values to match your VM.
+GROUP=WORKSTATION
+MACHINE=win10work2
+RDPCONFIG=~/.remmina/1518788004229.remmina
 
 REMMINA=$(which remmina)
 AZ=$(which az)
+ZENITY=$(which zenity)
 
 case "$1" in
   start)
-    echo "Starting Azure workstation..."
-    ${AZ} vm start -g WORKSTATION -n win10work2
-    echo "Connecting via RDP..."
-    ${REMMINA} -c ~/.remmina/1518788004229.remmina
+    echo "Starting Azure workstation and connecting via RDP."
+    #zenity --info --text="Starting Azure workstation" && \
+    ${AZ} vm start -g $GROUP -n $MACHINE |\
+    $ZENITY --progress --auto-close --pulsate --text="Starting Azure workstation" && \
+    ${REMMINA} -c $RDPCONFIG
     ;;
   stop)
     echo "Shutting down Azure workstation..."
-    ${AZ} vm stop -g WORKSTATION -n win10work2
+    ${AZ} vm stop -g $GROUP -n $MACHINE
     echo "Shutting down Remmina..."
     ${REMMINA} -q
     ;;
