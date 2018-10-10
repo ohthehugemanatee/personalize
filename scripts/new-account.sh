57f01e641b91590a74c7a1f4e006537e43032af8
Campbell Vertesi <campbell@vertesi.com>
2018-10-10 01:56:57 -0700

    Revert "Add tmux.conf"
    This reverts commit 9c456ffcd4e2b204a45ffc096ae6ac1f2799e3c4.

--- scripts/new-account.sh
+++ scripts/new-account.sh
@@ -12,4 +12,3 @@ mkdir .vagrant.d
 ln -s personalize/Vagrantfile .vagrant.d/Vagrantfile
 ln -s personalize/.selected-editor
 cd ~/.oh-my-zsh/custom && ln -sf personalize/zsh/custom/*
-ln -s personalize/.tmux.conf
