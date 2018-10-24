622a4a75fb7cddcac85018e69a9f5fc70f444120
Campbell Vertesi <campbell@vertesi.com>
2018-10-24 13:08:14 +0200

    Update screenshot.sh for nextcloud

--- linux/2017xps13/usr/local/bin/screenshot.sh
+++ linux/2017xps13/usr/local/bin/screenshot.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # take a screenshot, save it to Dropbox, copy the share link to the clipboard.
-FILENAME=~/Dropbox/Screenshots/Screenshot_`date +%Y-%m-%d-%H.%M`.png
+FILENAME=Screenshots/Screenshot_`date +%Y-%m-%d-%H.%M`.png
 
 # Select an area and save the screenshot. We pass our arguments directly to gnome-screenshot
-gnome-screenshot -f "$FILENAME" "$*"
-#sleep 3 
-URL=`dropbox sharelink "$FILENAME"`
+mkdir -p /tmp/Screenshots
+gnome-screenshot -f "/tmp/${FILENAME}" "$*"
+#URL=`dropbox sharelink "$FILENAME"`
+cloud-dl -u "/tmp/${FILENAME}" Screenshots
+URL=`cloud-dl -s "$FILENAME" -q`
 echo $URL | xsel -ib
 
 # Pop up a small notification
