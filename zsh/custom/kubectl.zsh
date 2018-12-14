d8f0815ac44db8c27b0dcb17cc7ba6efab06fc3d
Campbell Vertesi <campbell@vertesi.com>
2018-12-14 14:55:52 +0100

    Add kubectl, taskwarrior zsh autocomplete

---
+++ zsh/custom/kubectl.zsh
@@ -0,0 +1,2 @@
+source <(kubectl completion zsh)  # setup autocomplete in zsh into the current shell
+echo "if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi" >> ~/.zshrc # add autocomplete permanently to your zsh shell
---
+++ zsh/custom/taskwarrior.zsh
@@ -0,0 +1,5 @@
+# Taskwarrior integrations.
+# @see https://cs-syd.eu/posts/2015-06-21-gtd-with-taskwarrior-part-2-collection
+
+alias in='task add +inbox'
+alias inbox=in
