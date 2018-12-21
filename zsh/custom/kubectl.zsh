7bbc44fbd6a7b2af1636595a5957d04459226892
Campbell Vertesi <campbell@vertesi.com>
2018-12-21 13:55:42 +0100

    Fix kubectl initialization so it doesn't double

--- zsh/custom/kubectl.zsh
+++ zsh/custom/kubectl.zsh
@@ -1,2 +1,1 @@
 source <(kubectl completion zsh)  # setup autocomplete in zsh into the current shell
-echo "if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi" >> ~/.zshrc # add autocomplete permanently to your zsh shell
