5caa3ee95eada24dfb20ada342955f96f55d6ae8
Campbell Vertesi <campbell@vertesi.com>
2019-11-18 01:23:14 +0100

    remove unnecessary extra dircolors

--- zsh/custom/docker.zsh
+++ zsh/custom/docker.zsh
@@ -38,9 +38,6 @@ alias rbt="SIMPLETEST_BASE_URL=http://ras-backend.dork.io SIMPLETEST_DB=sqlite:/
 
 alias clipboard="xsel -ib"
 
-# color scheme for directories
-eval `dircolors $HOME/.dir_colors/dircolors`
-
 # Automatically added by Platform.sh CLI installer
 export PATH="/home/ohthehugemanatee/.platformsh/bin:$PATH"
 . '/home/ohthehugemanatee/.platformsh/shell-config.rc' 2>/dev/null
