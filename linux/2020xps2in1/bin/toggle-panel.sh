ececf7c04f1b11c31a9442e2c4463d5b40820d8e
Campbell Vertesi <campbell@vertesi.com>
2020-02-04 00:34:17 +0100

    Add onboard keyboard to toggle

--- linux/2020xps2in1/bin/toggle-panel.sh
+++ linux/2020xps2in1/bin/toggle-panel.sh
@@ -5,7 +5,9 @@ COMMAND="xfce4-panel"
 if pgrep $COMMAND > /dev/null
 then
   pkill $COMMAND
+  pkill onboard
   exit 0
 fi
 setsid /usr/bin/xfce4-panel --disable-wm-check & > /dev/null
+setsid /usr/bin/onboard & > /dev/null
 exit 0
