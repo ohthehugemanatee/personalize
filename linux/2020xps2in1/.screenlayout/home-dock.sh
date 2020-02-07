ecbc3606b7760997d189c69067f3b282aaf81dd8
Campbell Vertesi <campbell@vertesi.com>
2020-02-07 11:01:47 +0100

    Update home dock config for new laptop

--- linux/2020xps2in1/.screenlayout/home-dock.sh
+++ linux/2020xps2in1/.screenlayout/home-dock.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 3200x1800 --pos 10720x1368 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1080 --scale 2x2 --pos 0x1008 --rotate normal --output DP2-2 --off --output DP2-3 --off --output DP1-3 --off --output DP1-2 --primary --mode 3440x1440 --scale 2x2 --pos 3840x0 --rotate normal --output DP1-1 --off --output DP2 --off
+xrandr \
+  --output eDP-1 --primary --mode 3840x2400 --scale "1.3x1.3" --pos 4705x3456 --rotate normal \
+	--output DP-4-2 --mode 3440x1440 --scale "2.4x2.4" --pos 4608x0 --auto --rotate normal \
+	--output DP-3 --mode 1920x1080 --scale "2.4x2.4" --pos 0x1473 --rotate normal \
+	--output DP-4 --off \
+	--output DP-4-1 --off \
+	--output DP-4-3 --off \
+	--output DP-1 --off \
+	--output DP-2 --off
