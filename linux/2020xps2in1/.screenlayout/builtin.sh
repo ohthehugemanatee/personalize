f83382ccba2a0e076c9c8e0a578f88e4252beb3f
Campbell Vertesi <campbell@vertesi.com>
2020-02-07 12:56:15 +0100

    Add builtin screen layout

---
+++ linux/2020xps2in1/.screenlayout/builtin.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output VIRTUAL1 --off
