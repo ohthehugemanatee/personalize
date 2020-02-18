c2a0d81e0432c16278b2a61e21aa911c098e195f
Campbell Vertesi <campbell@vertesi.com>
2020-02-18 13:33:40 +0100

    Add hamburg meeting room screen

---
+++ linux/2020xps2in1/.screenlayout/hamburg-mtg-room.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x2484 --rotate normal --output DP-1 --mode 1920x1080 --pos 4119x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
