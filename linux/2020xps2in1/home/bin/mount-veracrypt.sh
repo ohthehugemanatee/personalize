8a1eacc7f0f505d2f3e9ccb01dcc252567996903
Campbell Vertesi <campbell@vertesi.com>
2020-11-16 14:34:45 +0100

    Add nokernelcrypto to veracrypt mount. :(

--- linux/2020xps2in1/home/bin/mount-veracrypt.sh
+++ linux/2020xps2in1/home/bin/mount-veracrypt.sh
@@ -2,4 +2,4 @@
 
 # Mount my veracrypt drive, using secrets from the keychain.
 
-veracrypt --use-dummy-sudo-password --password=$(secret-tool lookup title veracrypt1) --mount $PWD/Nextcloud/Campbells\ homedir/encrypted.tc /mnt/veracrypt1
+veracrypt --use-dummy-sudo-password --password=$(secret-tool lookup title veracrypt1) -m=nokernelcrypto --mount $HOME/Nextcloud/Campbells\ homedir/encrypted.tc /mnt/veracrypt1
