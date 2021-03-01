a21879556de68c3f2676b4db5153d3ecf5eb8d1a
Campbell Vertesi <campbell@vertesi.com>
2021-03-01 02:01:57 +0100

    Add pods-by-node alias

--- zsh/custom/kubectl.zsh
+++ zsh/custom/kubectl.zsh
@@ -1,3 +1,4 @@
 source <(kubectl completion zsh)  # setup autocomplete in zsh into the current shell
 alias k=kubectl
 export KUBE_EDITOR="vim"
+alias pods-by-node="kubectl get pod -o=custom-columns=NODE:.spec.nodeName,NAMESPACE:.metadata.namespace,NAME:.metadata.name"
