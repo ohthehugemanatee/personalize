e8c3dede0becefbcabe56a9a764ff30630aeff8c
Campbell Vertesi <campbell@vertesi.com>
2021-10-15 17:39:16 +0200

    Add notification to veracrypt mounter

--- linux/2021xps2in1/home/bin/mount-veracrypt.sh
+++ linux/2021xps2in1/home/bin/mount-veracrypt.sh
@@ -2,4 +2,5 @@
 
 # Mount my veracrypt drive, using secrets from the keychain.
 
-veracrypt --use-dummy-sudo-password --password=$(secret-tool lookup title veracrypt1) -m=nokernelcrypto --mount $HOME/Nextcloud/Campbells\ homedir/encrypted.tc /mnt/veracrypt1
+veracrypt --use-dummy-sudo-password --password=$(secret-tool lookup title veracrypt1) -m=nokernelcrypto --mount $HOME/Nextcloud/Campbells\ homedir/encrypted.tc /mnt/veracrypt1 && notify-send "Mounted veracrypt"
+
