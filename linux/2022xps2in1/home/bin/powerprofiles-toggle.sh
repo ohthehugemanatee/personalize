3eaa81b1c5a496696c0bb69344ca5f4d5c169d84
Campbell Vertesi <campbell@vertesi.com>
2022-09-21 14:57:12 -0700

    add powerprofiles toggle script

---
+++ linux/2022xps2in1/home/bin/powerprofiles-toggle.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# wrapper for powerprofilesctl to allow cycling through available options.
+# Passes arguments directly to powerprofilesctl except for "toggle",
+# which cycles through profiles.
+
+PPC=$(which powerprofilesctl)
+
+getoptions() {
+  OPTIONS=$(${PPC} list |sed 's/\s\s\s\s.*//g' |sed 's/^[ \*]*//g' | sed 's/:.*$//g')
+  echo $OPTIONS
+}
+
+toggle() {
+  OPTIONS=($(getoptions))
+  COUNT=${#OPTIONS[*]}
+  CURRENT=$(${PPC} get)
+  echo "current: ${CURRENT}"
+  for (( i=0; i<=$(( $COUNT -1 )); i++ ))
+  do
+    if [[ ${OPTIONS[${i}]} == ${CURRENT} ]]
+    then
+      if (( ${i} == $(( ${COUNT} -1 )) ))
+      then 
+        TARGET=${OPTIONS[0]}
+      else 
+        TARGET=${OPTIONS[(( ${i} +1 ))]}
+      fi
+      if [ -v TARGET ]
+      then
+        echo "Switching to ${TARGET}"
+        ${PPC} set ${TARGET}
+        return
+      fi
+      echo "No new target power state found. Exiting..."
+      return
+    fi
+  done
+
+}
+
+if [[ $1 == 'options' ]]
+then
+  getoptions
+  exit 0
+fi
+
+if [[ $1 == 'toggle' ]]
+then
+  toggle
+  exit 0
+fi
+
+$PPC $@
+
