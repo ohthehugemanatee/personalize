81bf061fd54e56353c3d6cb462ae47470d8aff13
Campbell Vertesi <campbell@vertesi.com>
2022-09-24 19:57:54 -0700

    Add tablet mode toggle to sway

---
+++ linux/2022xps2in1/home/bin/tablet-mode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Executes behaviors for tablet mode and non-tablet mode, based on the single argument 'on' or 'off'.
+
+# Enable tablet mode.
+if [[ $1 == 'on' ]]; then
+  notify-send "Tablet mode enabled"
+  pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.tablet.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.tablet.css"
+fi
+
+# disable tablet mode
+if [[ $1 == 'off' ]]; then
+  notify-send "Tablet mode disabled"
+  pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.css"
+fi
