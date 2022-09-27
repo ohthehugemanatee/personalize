2aacb1dcc6492a22eb5367fe77c058c86fd0a7f9
Campbell Vertesi <campbell@vertesi.com>
2022-09-27 15:30:16 -0400

    Change display scale in tablet mode

--- linux/2022xps2in1/home/bin/tablet-mode.sh
+++ linux/2022xps2in1/home/bin/tablet-mode.sh
@@ -5,13 +5,13 @@
 # Enable tablet mode.
 if [[ $1 == 'on' ]]; then
   notify-send "Tablet mode enabled"
-  pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.tablet.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.tablet.css"
-  swaymsg eDP-1 scale 3
+  pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.tablet.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.tablet.css" &
+  /usr/bin/swaymsg output eDP-1 scale 3
 fi
 
 # disable tablet mode
 if [[ $1 == 'off' ]]; then
   notify-send "Tablet mode disabled"
-  pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.css"
-  swaymsg eDP-1 scale 2
+  pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.css" &
+  /usr/bin/swaymsg output eDP-1 scale 2
 fi
