0b57cc55abf739ba8d0bd1c7d1cbf1494c373d87
Campbell Vertesi <campbell@vertesi.com>
2022-09-27 15:01:00 -0400

    Scale output with tablet mode

--- linux/2022xps2in1/home/bin/tablet-mode.sh
+++ linux/2022xps2in1/home/bin/tablet-mode.sh
@@ -6,10 +6,12 @@
 if [[ $1 == 'on' ]]; then
   notify-send "Tablet mode enabled"
   pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.tablet.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.tablet.css"
+  swaymsg eDP-1 scale 3
 fi
 
 # disable tablet mode
 if [[ $1 == 'off' ]]; then
   notify-send "Tablet mode disabled"
   pkill waybar; waybar -b default -c "/home/ohthehugemanatee/.config/waybar/config.jsonc" -s "/home/ohthehugemanatee/.config/waybar/style.css"
+  swaymsg eDP-1 scale 2
 fi
