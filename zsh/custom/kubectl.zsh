3e9a9d0ec6628abc39a1a6b49315d2301be1aa3b
Campbell Vertesi <campbell@vertesi.com>
2023-02-01 13:45:14 +0100

    Remove redundant alias, completion for kubectl

--- zsh/custom/kubectl.zsh
+++ zsh/custom/kubectl.zsh
@@ -1,4 +1,2 @@
-source <(kubectl completion zsh)  # setup autocomplete in zsh into the current shell
-alias k=kubectl
 export KUBE_EDITOR="vim"
 alias pods-by-node="kubectl get pod -o=custom-columns=NODE:.spec.nodeName,NAMESPACE:.metadata.namespace,NAME:.metadata.name"
