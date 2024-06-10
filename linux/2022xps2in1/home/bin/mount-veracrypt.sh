943829b9f0837f3381805a862e82508c3e214f91
Campbell Vertesi <campbell@vertesi.com>
2024-06-10 11:55:22 +0200

    Stop preserving timestamp on veracrypt containers so they sync properly

--- linux/2022xps2in1/home/bin/mount-veracrypt.sh
+++ linux/2022xps2in1/home/bin/mount-veracrypt.sh
@@ -2,5 +2,5 @@
 
 # Mount my veracrypt drive, using secrets from the keychain.
 
-/usr/bin/veracrypt -t --non-interactive --password=$(secret-tool lookup title veracrypt1) --use-dummy-sudo-password  -m=nokernelcrypto --mount /home/ohthehugemanatee/Nextcloud/Campbells\ homedir/encrypted.tc /mnt/veracrypt1 
+/usr/bin/veracrypt -t --non-interactive --password=$(secret-tool lookup title veracrypt1) --use-dummy-sudo-password  -m=timestamp -m=nokernelcrypto --mount /home/ohthehugemanatee/Nextcloud/Campbells\ homedir/encrypted.tc /mnt/veracrypt1 
 
